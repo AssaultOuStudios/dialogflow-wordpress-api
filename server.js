ad9b80b9e60604148afbece8ec590a45d3ba0e05
Vernon Joyce <vernon@assaultou.com>
2017-12-29 15:56:12 +0200

    Updates

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-// Modules
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// Imports
-//const chatbase = require(__dirname + '/lib/chatbase/chatbase');
-
-// Set port for server
 const port = process.env.PORT || 3100;
 
-// Express set up
 let app = express();
 app.use(bodyParser.json());
 
-// Receive a message and send to API.ai
 app.post('/webhook', (req, res, next) => {
+	
+	let action = req.body.result.action;
+	let message = action === 'get.wp.content' ? `Hey, our webhook is connected!` : `Sorry, I didn't get that`;
+	
+	res.send({
+		speech: message,
+		displayText: message,
+		source: 'wp-webhook',
+	});
 	
 });
 
-// Start server
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`)
 });
