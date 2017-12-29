1fd0bf8c13e07ff05c690cd55085afe51db8051d
Vernon Joyce <vernon@assaultou.com>
2017-12-29 15:14:12 +0200

    Init

---
+++ server.js
@@ -0,0 +1,23 @@
+// Modules
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Imports
+const chatbase = require(__dirname + '/lib/chatbase/chatbase');
+
+// Set port for server
+const port = process.env.PORT || 3100;
+
+// Express set up
+let app = express();
+app.use(bodyParser.json());
+
+// Receive a message and send to API.ai
+app.post('/webhook', (req, res, next) => {
+	
+});
+
+// Start server
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+});
